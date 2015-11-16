7236f3616a14c586ba99e3cc991d387bc6c2813c
flybywind@foxmail.com <flybywind@foxmail.com>
2015-11-17 07:47:14 +0800

    add table cell with init method

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: table view delegate
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 相当于创建了一个cell，identifier为MyTestCell；
+        // 和FoodTracker中在IB中创建cell prototype不同，那种cell要通过tableView.dequeueReusableCellWithIdentifier(_:forIndexPath:)获得，然后转化成我们需要的类型(as! MealTableViewCell)
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+            
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        return cell
+    }
 }
 
